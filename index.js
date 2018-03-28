948a080799cb6473ccf13639e5468032dd82bea0
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-28 14:45:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = []
+
+function takeANumber (line, name) {
+  line.push(name)
+  return 'Welcome, ' + name + '. You are number ' + (line.indexOf(name)+1) + ' in line.'
+}
+
+function nowServing (line) {
+  for (line.length; line.length >= 0 ; line.shift()) {
+    if (line.length === 0 ) {
+      return "There is nobody waiting to be served!"
+    } else {
+      return "Currently serving " + line.shift() + "."
+    }
+  }
+}
+
+function currentLine (line) {
+  var arr = "";
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0 ; i < line.length; i++) {
+      arr += ((i+1) + ". " + line[i] + ", ")
+    } 
+    return "The line is currently: " + arr.slice(0, -2)
+  }
+}
